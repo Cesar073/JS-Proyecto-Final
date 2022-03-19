90ae90951b9ca8f9ade7c0ea41cb198da930b9c9
Cesar0007 <cesar_baby@hotmail.com>
2022-03-19 00:16:45 -0300

    Animacion banner 1

--- scripts/script_home.js
+++ scripts/script_home.js
@@ -3,8 +3,11 @@ let pos_banner = 1;
 $('#nav_right').click(()=> {
     switch(pos_banner){
         case 1:
-            $('#banner2').animate({left: '250px'});
+            $("#banner1").animate({right: '1250px'}).fadeOut(0);
             pos_banner += 1;
+            $("#banner2").css("margin-left","1300px");
+            $("#banner2").css("visibility","visible").fadeIn(0);
+            $("#banner1").animate({right: '1250px'}).fadeOut(0);
             break;
         case 2:
             pos_banner += 1;
